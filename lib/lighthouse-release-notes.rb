af21782715caeff0d5146ead703b3309738d418d
Jeff Jolma <jeff@animoto.com>
2009-05-07 11:53:32 -0400

    add :project_id option

--- lib/lighthouse-release-notes.rb
+++ lib/lighthouse-release-notes.rb
@@ -4,22 +4,26 @@ require 'lighthouse-api'
 class LighthouseReleaseNotes
   class << self
     def generate(options={})
+
       Lighthouse.account = options[:account]
-      Lighthouse.authenticate(options[:email], options[:password])
-      web2 = Lighthouse::Project.find(7135)
+      Lighthouse.authenticate options[:email], options[:password]
+      project = Lighthouse::Project.find options[:project_id]
+
       tickets = []
       i = 1
       while true
-        ts = web2.tickets(:q => "milestone:#{options[:milestone]}", :page => i)
+        ts = project.tickets(:q => "milestone:#{options[:milestone]}", :page => i)
         break if ts.size <= 0
         tickets << ts
         i = i + 1
       end
+
+      puts "Release notes for #{options[:milestone]}"
       prev_state = nil
       tickets.flatten.sort {|a,b| b.state <=> a.state }.each do |f|
         if f.state != prev_state
           puts ""
-          puts "State: #{f.state}"
+          puts f.state.capitalize
         end
         puts "- #{f.title} [##{f.number}]"
         prev_state = f.state
